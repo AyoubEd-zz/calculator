380d57ed3d9114287507dfcb61258edd4ec1b41b
Your Name <ayoube.eddakhly@gmail.com>
2022-07-23 11:27:19 -0700

    Calc2.py

--- Calc2.py
+++ Calc2.py
@@ -1,19 +1,98 @@
+import math
+
 class Add:
-    def addition(self, a, b):
+    def addition(self):
+        a = int(input("Enter first number: "))
+        b = int(input("Enter second number: "))
         return a+b
 class Sub:
-    def subtract(self, a, b):
+    def subtract(self):
+        a = int(input("Enter first number: "))
+        b = int(input("Enter second number: "))
         return a - b
 class Mult:
-    def multiply(self, a, b):
+    def multiply(self):
+        a = int(input("Enter first number: "))
+        b = int(input("Enter second number: "))
         return a * b
 class Div:
-    def division(self, a,b):
+    def division(self):
+        a = int(input("Enter first number: "))
+        b = int(input("Enter second number: "))
         return a/b
 class Mod:
-    def modulus(self,a,b):
+    def modulus(self):
+        a = int(input("Enter first number: "))
+        b = int(input("Enter second number: "))
         return a%b
-class All(Add,Sub,Mult,Mod,Div):
+class Exp:
+    def exp(self):
+        Base=float(input("\n Enter the Base NUmber whose powers you have to calculate : "))
+        exponent=float(input("\n Enter the exponent Number which will be given as power to base  : "))
+        power=Base**exponent
+        return power
+class Root:
+    def root(self):
+        Number=float(input("\n Enter the number whose root you want to find : "))
+        exponent=float(input("\n Which Root You want ? "))
+        result=pow(Number,1/exponent)
+        return result
+class Factorial:
+    def fact(self):
+        factorial=1
+        Number=float(input("\n Enter a Number whose factorial You want to be Calculated : "))
+        if (Number>=1) and Number.is_integer()==True:
+            for fact in range(1, int(Number+1)):
+                factorial=factorial*fact
+            return factorial
+        else:
+            print("\n")
+            for i in tqdm (range (100), desc=" ERROR !!!!..."):
+                time.sleep(0.01)
+                pass
+            print("\n The FACTORIAL cannot be calculated for NEGATIVE NUMBERS and DECIMAL NUMBERS !!!!!!!!!", end="\n\n")
+        return "NULL"
+class Trig:
+    def trig(self):
+        print("\n WHICH TRIGNOMETRIC FUNCTION DO YOU WANT TO USE \n\n 1. SIN \n 2. COS \n 3. TAN \n 4. SEC \n 5. COSEC \n 6. COT ", end="\n")
+        choice=float(input("\n Enter Choice (1-6)"))
+        x=float(input("\n Enter the values in degrees "))
+        x=(x/180)*3.14159265359
+        if (choice==1):
+            return math.sin(x)
+        elif(choice==2):
+            return math.cos(x)
+        elif(choice==3):
+            return math.tan(x)
+        elif(choice==4):
+            return 1/math.cos(x)
+        elif(choice==5):
+            return 1/math.sin(x)
+        elif(choice==6):
+            return 1/math.tan(x)
+        else:
+            print("\n\n INVALID CHOICE !!!! \n \n Please Enter a VALID CHOICE(1-6)",end="\n ")
+        return(0)
+
+class Logarithm:
+    def log(self):
+        print("\n Which type of logarithms do you want to calculate ?", end="\n")
+        print("\n 1.  NATURAL LOGS , (Base=e or ln)", end="\n")
+        print("\n 2. LOGS WITH A BASE AND VALUE", end="\n")
+        choice=int(input("\n Enter 1 or 2 "))
+        if choice==1:
+            Base=float(input("\n Enter a Base Number whose log you want to calculate "))
+            return math.log(Base)
+        elif choice==2:
+            Base=float(input("\n Enter a Base Number whose log you want to calculate "))
+            Exponent=float(input("\n Enter a exponent number which will the log base "))
+            return math.log(Base, Exponent)
+        else:
+            print("\n Print INVALID CHOICE!!!! \n\n")
+            print("Enter a choice between 1 or 2 ")
+        return 0
+
+class All(Add,Sub,Mult,Mod,Div,Exp, Root, Factorial, Trig, Logarithm):
     pass
 
 Math = All()
@@ -25,31 +104,42 @@ while True:
     print("3: Multiply")
     print("4: Divide")
     print("5: Modulus ")
-    print("6: Exit ")
+    print("6: Exp")
+    print("7: Root")
+    print("8: Factorial")
+    print("9: Trigonometry")
+    print("10: Logarithm")
+    print("0: Exit ")
 
     cd = int(input("Select operation: "))
 
     # Make sure the user have entered the valid choice
-    if cd in (1, 2, 3, 4, 5, 6):
+    if cd <= 10:
 
         # first check whether user want to exit
-        if (cd == 6):
+        if (cd == 0):
             break
 
-        # If not then ask fo the input and call appropiate methods
-        a = int(input("Enter first number: "))
-        b = int(input("Enter second number: "))
-
         if (cd == 1):
-            print(a, "+", b, "=", Math.addition(a,b))
+            print("Result", Math.addition())
         elif (cd == 2):
-            print(a, "-", b, "=", Math.subtract(a, b))
+            print("Result", Math.subtract())
         elif (cd == 3):
-            print(a, "*", b, "=", Math.multiply(a, b))
+            print("Result", Math.multiply())
         elif (cd == 4):
-            print(a, "%", b, "=", Math.division(a, b))
+            print("Result", Math.division())
         elif (cd == 5):
-            print(a, "\ ", b , "=",Math.modulus(a,b))
+            print("Result", Math.modulus())
+        elif cd == 6:
+            print("Result", Math.exp())
+        elif cd == 7:
+            print("Result", Math.root())
+        elif cd == 8:
+            print("Result", Math.fact())
+        elif cd == 9:
+            print("Result", Math.trig())
+        elif cd == 10:
+            print("Result", Math.log())
 
     else:
         print("Invalid Input")
